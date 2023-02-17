b2ead5ffd9d85a11b2e4cf3eaaf84e8e7bbad5bd
Alex <lex.courtney18@gmail.com>
2023-02-17 14:49:14 -0600

    pass data through as props from App parent down to Modal child

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,6 @@
 <template>
   <h1>{{ title }}</h1>
-  <Modal />
-  <input type='text' ref='name'>
-  <button @click="handleClick">Click Me</button>
+  <Modal :header="header" :text="text" theme="sale" />
 </template>
 
 <script>
@@ -14,7 +12,9 @@ export default {
   components: { Modal },
   data() {
     return {
-      title: 'My First Vue App :)'
+      title: 'My First Vue App :)',
+      header: 'Sign up for the Giveaway!',
+      text: 'Grab your swag for half price!'
     }
   },
   methods: {
